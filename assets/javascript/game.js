57dcc6512ad5149b5cb84b73e75bba0e9019690a
Erika Martinez <erikamartinez@Erikas-MacBook-Pro.local>
2017-07-11 23:34:54 -0700

    adding css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,10 @@ console.log(targetNumber);
   var counter = 0;
   var wonGames = 0;
   var lostGames = 0;
-  var yellow = Math.floor(Math.random()*9+4);
-  var blue = Math.floor(Math.random()*15+9);
-  var pink = Math.floor(Math.random()*4+1);
-  var green = Math.floor(Math.random()*11+1);
+  var yellow = Math.floor(Math.random()*3+1);
+  var blue = Math.floor(Math.random()*5+3);
+  var pink = Math.floor(Math.random()*7+5);
+  var green = Math.floor(Math.random()*11+7);
 
 
     $('#yellow').on ('click', function(){
@@ -18,9 +18,11 @@ console.log(targetNumber);
     $('#finalScore').text(counter); 
         if (counter === targetNumber){
           wonGames++;
+          yellow = Math.floor(Math.random()*3+1);
         }
         else if ( counter > targetNumber){
           lostGames++;
+          yellow = Math.floor(Math.random()*3+1);
         }   
   }) 
 
@@ -30,9 +32,11 @@ console.log(targetNumber);
     $('#finalScore').text(counter); 
         if (counter === targetNumber){
           wonGames++;
+          blue = Math.floor(Math.random()*5+3);
         }
         else if ( counter > targetNumber){
           lostGames++;
+          blue = Math.floor(Math.random()*5+3);
         }   
   }) 
 
@@ -42,9 +46,11 @@ console.log(targetNumber);
     $('#finalScore').text(counter); 
         if (counter === targetNumber){
           wonGames++;
+          pink = Math.floor(Math.random()*7+5);
         }
         else if ( counter > targetNumber){
           lostGames++;
+          pink = Math.floor(Math.random()*7+5);
         }   
   }) 
 
@@ -54,9 +60,11 @@ console.log(targetNumber);
     $('#finalScore').text(counter); 
         if (counter === targetNumber){
           wonGames++;
+          green = Math.floor(Math.random()*11+7);
         }
         else if ( counter > targetNumber){
           lostGames++;
+          green = Math.floor(Math.random()*11+7);
         }   
   }) 
 
@@ -84,4 +92,34 @@ console.log(targetNumber);
     }
 
 });
+
+  //New game
+
+  $("#newGame").click(startNewGame);
+
+  function startNewGame() {
+
+    targetNumber = Math.floor((Math.random() * 75) + 15);
+    counter = "";
+    wonGames = "";
+    lostGames = "";
+    yellow = Math.floor(Math.random()*3+1);
+    blue = Math.floor(Math.random()*5+3);
+    pink = Math.floor(Math.random()*7+5);
+    green = Math.floor(Math.random()*11+7);
+
+  $("#random-number").text(targetNumber);
+  $("#wins").text(wonGames);
+  $("#finalScore").text(counter);
+  $("#loses").text(lostGames);
+
+  }
+
+
+
+
+
+
+
+
 
